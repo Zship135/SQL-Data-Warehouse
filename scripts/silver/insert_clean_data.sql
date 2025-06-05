9feead377903637ab87e32e22ce85bf380a19279
Zachary Shipley <114198074+Zship135@users.noreply.github.com>
2025-06-04 23:04:21 -0400

    Update insert_clean_data.sql

--- scripts/silver/insert_clean_data.sql
+++ scripts/silver/insert_clean_data.sql
@@ -1,4 +1,9 @@
+/*
+===============================================================
+Insert Clean Data Into Silver Layer
+===============================================================
 
+*/
 ----------------------------------------------
 -- crm_cust_info --
 ----------------------------------------------
@@ -16,12 +21,14 @@ cust_id,
 cust_key, 
 TRIM(cust_firstname), 
 TRIM(cust_lastname), 
-CASE WHEN UPPER(TRIM(cust_marital_status)) = 'S' THEN 'Single'
-	 WHEN UPPER(TRIM(cust_marital_status)) = 'M' THEN 'Married'
+CASE UPPER(TRIM(cust_marital_status))
+	 WHEN 'S' THEN 'Single'
+	 WHEN 'M' THEN 'Married'
 	 ELSE 'n/a'
 END cust_marital_status,
-CASE WHEN UPPER(TRIM(cust_gender)) = 'F' THEN 'Female'
-	 WHEN UPPER(TRIM(cust_gender)) = 'M' THEN 'Male'
+CASE UPPER(TRIM(cust_gender))
+	 WHEN 'F' THEN 'Female'
+	 WHEN 'M' THEN 'Male'
 	 ELSE 'n/a'
 END cust_gender,
 cust_create_date
@@ -30,3 +37,35 @@ FROM (
 	FROM bronze.crm_cust_info
 	WHERE cust_id IS NOT NULL
 ) WHERE last_flag = 1;
+
+----------------------------------------------
+-- crm_product_info --
+----------------------------------------------
+
+INSERT INTO silver.crm_product_info (
+product_id,
+cat_id,
+product_key,
+product_name,
+product_cost,
+product_line,
+product_start_date,
+product_end_date
+)
+SELECT
+product_id,
+REPLACE(SUBSTRING(product_key, 1, 5), '-', '_') AS cat_id,
+SUBSTRING(product_key, 7, LENGTH(product_key)) AS product_key,
+product_name,
+COALESCE(product_cost, 0) AS product_cost,
+CASE UPPER(TRIM(product_line))
+	 WHEN 'M' THEN 'Mountain'
+	 WHEN 'R' THEN 'Road'
+	 WHEN 'S' THEN 'Other Sales'
+	 WHEN 'T' THEN 'Touring'
+	 ELSE 'n/a'
+END AS product_line,
+CAST(product_start_date AS DATE) AS product_start_date,
+CAST(LEAD (product_start_date) OVER (PARTITION BY product_key ORDER BY product_start_date)-1 AS DATE) AS product_end_date
+FROM bronze.crm_product_info;
+
